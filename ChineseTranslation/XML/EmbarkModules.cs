f737ae6ff2599a9ea108c4dcb15e7b26ab050abc
Wenxuan Zhao <viz@linux.com>
2024-12-10 09:07:15 +0800

    WIP

--- ChineseTranslation/XML/EmbarkModules.cs
+++ ChineseTranslation/XML/EmbarkModules.cs
@@ -1,29 +1,45 @@
+using System;
 using System.Collections.Generic;
 using HarmonyLib;
 using XRL.CharacterBuilds;
-using XRL.CharacterBuilds.Qud;
-using static XRL.CharacterBuilds.Qud.QudGamemodeModule;
+using XRL.CharacterBuilds.Qud.UI;
+using XRL.UI.Framework;
 
 namespace Lofucc.ChineseTranslation.XML.EmbarkModules
 {
-  [HarmonyPatch(typeof(AbstractEmbarkBuilderModule), "HandleWindowTitleNode")]
-  class AbstractEmbarkBuilderModule__HandleWindowTitleNode
+  [HarmonyPatch(typeof(FrameworkScroller), "BeforeShow", new Type[] { typeof(EmbarkBuilderModuleWindowDescriptor), typeof(IEnumerable<FrameworkDataElement>) })]
+  class FrameworkScroller__BeforeShow
   {
-    static void Postfix(EmbarkBuilderModuleWindowDescriptor ___CurrentLoadingWindowDescriptor, object xml)
+    static void Prefix(EmbarkBuilderModuleWindowDescriptor descriptor)
     {
-      Dict.Translate(ref ___CurrentLoadingWindowDescriptor.title, "XML/Embark/Window/Title");
+      if (descriptor != null)
+      {
+        Dict.Translate(ref descriptor.title, "XML/Embark/Window/Title");
+      }
     }
   }
 
-  [HarmonyPatch(typeof(QudGamemodeModule), "Init")]
-  class QudGamemodeModule__Init
+  [HarmonyPatch(typeof(QudGamemodeModuleWindow), "BeforeShow")]
+  class QudGamemodeModuleWindow__BeforeShow
   {
-    static void Postfix(Dictionary<string, GameModeDescriptor> ___GameModes)
+    static void Prefix(QudGamemodeModuleWindow __instance)
     {
-      foreach (var mode in ___GameModes.Values)
+      foreach (var mode in __instance.module.GameModes.Values)
       {
         Dict.Translate(ref mode.Title, "XML/Embark/Module/Gamemode/Title");
         Dict.Translate(ref mode.Description, "XML/Embark/Module/Gamemode/Description");
+      }
+    }
+  }
+
+  [HarmonyPatch(typeof(QudChartypeModuleWindow), "BeforeShow")]
+  class QudChartypeModuleWindow__BeforeShow
+  {
+    static void Prefix(QudChartypeModuleWindow __instance)
+    {
+      foreach (var type in __instance.module.GameTypes.Values)
+      {
+        Dict.Translate(ref type.Title, "XML/Embark/Module/Chartype/Title");
       }
     }
   }
